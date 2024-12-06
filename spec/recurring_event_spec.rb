5b790918c4d27cecd189f97dec290dae14ee40f1
sehoon.gim <sehoongim@gmail.com>
2024-12-06 16:58:55 +0900

    Person 객체가 이벤트 발생 Date를 가지고 있는 경우

--- spec/recurring_event_spec.rb
+++ spec/recurring_event_spec.rb
@@ -3,4 +3,36 @@ describe "recurring event" do
   end
 
   it "schedule, schedule_element, temporal_expression 객체로 구성된다"
+
+  it "my friend mark" do
+    mark = Person.new("mark")
+    mark.add_evt(Event.new("1st and 3rd mon of month", "gastro clinic"))
+    mark.add_evt(Event.new("2nd wed of month", "liver clinic"))
+    mark.add_evt(Event.new("every mon", "golf games"))
+    p mark.events
+  end
+end
+
+class Person
+  attr_reader :events
+
+  def initialize name
+    @name = name
+    @events = []
+  end
+
+  def add_evt evt
+    @events << evt
+  end
+end
+
+class Event
+  def initialize occurrences, desc
+    @occurrences = conv2dates(occurrences)
+    @desc = desc
+  end
+
+  def conv2dates ocrs
+    ocrs
+  end
 end
