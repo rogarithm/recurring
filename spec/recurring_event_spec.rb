1901cb486c083bda7c174e753b9d713627cb466b
sehoon.gim <sehoongim@gmail.com>
2024-12-06 16:26:39 +0900

    반복 이벤트 생성에 참여할 객체 스펙을 작성한다

---
+++ spec/recurring_event_spec.rb
@@ -0,0 +1,6 @@
+describe "recurring event" do
+  it "street cleaning outside my old house occurs on the first and third Monday of the month" do
+  end
+
+  it "schedule, schedule_element, temporal_expression 객체로 구성된다"
+end
