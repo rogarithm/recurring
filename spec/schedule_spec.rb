3128085e49db9925648307ba6fccca93f17b71d3
sehoon.gim <sehoongim@gmail.com>
2024-12-09 14:51:58 +0900

    테스트 레이어를 나눈다
     - 테스트하려는 객체만 실제 객체를 쓰도록 바꾼다
     - schedule_spec에선 협력 객체인 schedule_elem과 temporal_expr를 test
       double로 대체한다
     - schedule_elem_spec에선 실제 schedule_elem 타입 객체를 쓰는 spec을
       작성한다

--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -1,46 +1,61 @@
 require_relative '../lib/schedule'
 require_relative '../lib/event'
-require_relative '../lib/schedule_elem'
-require_relative '../lib/temporal_expr'
 
 describe "schedule 객체는" do
-  it "어떤 날에 어떤 이벤트가 있는지 알 수 있다" do
-
+  RSpec.configure do |config|
+    config.filter_run_when_matching(focus: true)
+    config.example_status_persistence_file_path = 'spec/pass_fail_history'
   end
 
   it "주어진 기간 내에 찾으려는 이벤트가 예정된 날짜 목록을 알 수 있다" do
     scd = Schedule.new
-    scd.add_elem(
-      ScheduleElem.new(
-        Event.new("every mon", "golf games"),
-        TemporalExpr.new
-      )
+    schedule_elem = double()
+    # scd.add_elem(
+    #   ScheduleElem.new(
+    #     Event.new("every mon", "golf games"),
+    #     TemporalExpr.new
+    #   )
+    # )
+    scd.add_elem(schedule_elem)
+
+    allow(scd).to receive(:occurrences) {["2024-11-18","2024-11-25","2024-12-02"]}
+    expect(
+      scd.occurrences("golf games", ["2024-11-10","2024-12-03"])
+    ).to eq(
+      ["2024-11-18","2024-11-25","2024-12-02"]
     )
-    scd.occurrences("golf games", ["2024-11-10","2024-12-31"])
     # should return uniq set of mondays from given date range
   end
 
   it "주어진 날짜 다음에 찾으려는 이벤트가 예정된 날짜를 알 수 있다" do
     scd = Schedule.new
-    scd.add_elem(
-      ScheduleElem.new(
-        Event.new("1st and 3rd mon of month", "gastro clinic"),
-        TemporalExpr.new
-      )
-    )
-    scd.next_occurrence("gastro clinic", "2024-11-10")
+    schedule_elem = double()
+    # scd.add_elem(
+    #   ScheduleElem.new(
+    #     Event.new("1st and 3rd mon of month", "gastro clinic"),
+    #     TemporalExpr.new
+    #   )
+    # )
+    scd.add_elem(schedule_elem)
+
+    allow(scd).to receive(:next_occurrence) {"2024-11-18"}
+    expect(scd.next_occurrence("gastro clinic", "2024-11-10")). to eq("2024-11-18")
     # should return next 1st or 3rd monday
   end
 
   it "주어진 날짜에 찾으려는 이벤트가 예정되어 있는지 알 수 있다" do
     scd = Schedule.new
-    scd.add_elem(
-      ScheduleElem.new(
-        Event.new("every mon", "golf games"),
-        TemporalExpr.new
-      )
-    )
-    scd.is_occurring("golf games", "2024-12-02")
-    # should return true, because 2024-12-02 is monday
+    schedule_elem = double()
+    # scd.add_elem(
+    #   ScheduleElem.new(
+    #     Event.new("every mon", "golf games"),
+    #     TemporalExpr.new
+    #   )
+    # )
+    scd.add_elem(schedule_elem)
+
+    allow(scd).to receive(:is_occurring) {true}
+
+    expect(scd.is_occurring("golf games", "2024-12-02")).to eq(true)
   end
 end
