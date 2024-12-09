5d94707138d3a28d24ddaf5697096620ea584caa
sehoon.gim <sehoongim@gmail.com>
2024-12-09 17:11:17 +0900

    주어진 날짜의 요일과 몇번째 주에 속하는지 확인하는 temporal_expr을 구현한다

--- lib/temporal_expr.rb
+++ lib/temporal_expr.rb
@@ -1,5 +1,46 @@
 class TemporalExpr
   def includes date
+    raise 'this method should be overriden'
+  end
+end
 
+class DayInMonth < TemporalExpr
+  def initialize day_idx, cnt
+    @day_idx = day_idx
+    @cnt = cnt
+  end
+
+  def includes date
+    day_matches date and week_matches date
+  end
+
+  def day_matches date
+    date.cwday == @day_idx
+  end
+
+  def week_matches date
+    if @cnt > 0
+      week_from_start_matches date
+    else
+      week_from_end_matches date
+    end
+  end
+
+  def week_from_start_matches date
+    week_in_month(date.mday) == @cnt
+  end
+
+  def week_from_end_matches date
+    days_from_month_end = days_left_in_month(date) + 1
+    week_in_month(days_from_month_end) == @cnt.abs
+  end
+
+  def week_in_month day_num
+    ((day_num - 1) / 7) + 1
+  end
+
+  def days_left_in_month date
+    days_in_month = Date.new(date.year, date.mon, -1).day
+    days_in_month - date.mday
   end
 end
--- spec/temporal_expr_spec.rb
+++ spec/temporal_expr_spec.rb
@@ -1,4 +1,11 @@
+require 'date'
+
 describe "temporal expression" do
+  RSpec.configure do |config|
+    config.filter_run_when_matching(focus: true)
+    config.example_status_persistence_file_path = 'spec/pass_fail_history'
+  end
+
   it "주어진 날이 자신(temporal expr)에 속하는지 판단할 수 있다" do
 
   end
@@ -13,5 +20,56 @@ describe "temporal expression" do
 
   it "range every year temporal expr을 정의할 수 있다" do
     # 시작과 끝에 해당하는 day, month를 인자로 받는다
+  end
+
+  it "주어진 date 객체가 몇요일인지 확인할 수 있다" do
+    dim = DayInMonth.new(1, 1)
+    monday = Date.new(2024, 11, 11)
+    expect(dim.day_matches monday).to eq(true)
+  end
+
+  it "첫 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
+    dim = DayInMonth.new(1, 2)
+    monday = Date.new(2024, 11, 11)
+    expect(dim.week_matches monday).to eq(true)
+  end
+
+  it "마지막 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
+    dim = DayInMonth.new(1, -1)
+    friday = Date.new(2024, 11, 29)
+    expect(dim.week_matches friday).to eq(true)
+  end
+
+  it "Date#mday 주어진 date 객체가 표현하는 날짜가 달의 몇번째 날인지 알 수 있다" do
+    monday = Date.new(2024, 11, 11)
+    expect(monday.mday).to eq(11)
+  end
+
+  it "주어진 day_num(Date 객체가 표현하는 날짜가 해당 달에서 몇번째 날인지를 표현하는 값)이 몇번째 주에 속하는지 알 수 있다" do
+    ignore1, ignore2 = 1, 1
+    dim = DayInMonth.new(ignore1, ignore2)
+
+    (1..7).each {|first_week_day|
+      expect(dim.week_in_month first_week_day).to eq(1)
+    }
+    (8..14).each {|second_week_day|
+      expect(dim.week_in_month second_week_day).to eq(2)
+    }
+    (15..21).each {|third_week_day|
+      expect(dim.week_in_month third_week_day).to eq(3)
+    }
+    (22..28).each {|fourth_week_day|
+      expect(dim.week_in_month fourth_week_day).to eq(4)
+    }
+    (29..31).each {|fifth_week_day|
+      expect(dim.week_in_month fifth_week_day).to eq(5)
+    }
+  end
+
+  it "주어진 날짜에서, 해당 달의 마지막 날까지 남은 날의 수를 구할 수 있다" do
+    ignore1, ignore2 = 1, 1
+    dim = DayInMonth.new(ignore1, ignore2)
+    monday = Date.new(2024, 11, 11)
+    expect(dim.days_left_in_month monday).to eq(19)
   end
 end
