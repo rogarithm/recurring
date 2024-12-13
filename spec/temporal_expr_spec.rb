34415e606a3024809b0c34c57c4069f952aa5110
sehoon.gim <sehoongim@gmail.com>
2024-12-13 12:36:25 +0900

    테스트 정리한다
     - 테스트를 이해하기 용이하게 변수명을 고친다
     - 테스트 설명을 다듬는다

--- spec/temporal_expr_spec.rb
+++ spec/temporal_expr_spec.rb
@@ -7,57 +7,61 @@ describe "temporal expression" do
     it "기본 메서드를 갖는 temporal expr이 있고, 더 세부적인 질문에 답할 수 있는 메서드를 갖는 temporal expr의 구현 클래스가 있다"
   end
 
-  context "day every month temporal expr을 정의할 수 있다" do
+  context "day in month temporal expr을 정의할 수 있다" do
     # day of week와 count를 인자로 받는다
-    it "주어진 date 객체가 몇요일인지 확인할 수 있다" do
-      day_in_mon = DayInMonth.new(1, 1)
+    it "주어진 date의 요일을 알 수 있다" do
+      monday_in_1st_week = DayInMonth.new(1, 1)
       monday = Date.new(2024, 11, 11)
-      expect(day_in_mon.day_matches monday).to eq(true)
+      expect(monday_in_1st_week.day_matches monday).to eq(true)
     end
 
-    it "첫 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
-      day_in_mon = DayInMonth.new(1, 2)
-      monday = Date.new(2024, 11, 11)
-      expect(day_in_mon.week_matches monday).to eq(true)
+    it "첫번째 주를 기준으로 주어진 date가 달의 몇번째 주에 속하는지 알 수 있다" do
+      monday_in_2nd_week = DayInMonth.new(1, 2)
+
+      nov_1st_mon = Date.new(2024, 11, 4)
+      expect(monday_in_2nd_week.week_matches nov_1st_mon).to eq(false)
+
+      nov_2nd_mon = Date.new(2024, 11, 11)
+      expect(monday_in_2nd_week.week_matches nov_2nd_mon).to eq(true)
     end
 
-    it "마지막 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
-      day_in_mon = DayInMonth.new(1, -1)
-      friday = Date.new(2024, 11, 29)
-      expect(day_in_mon.week_matches friday).to eq(true)
+    it "마지막 주를 기준으로 주어진 date가 달의 몇번째 주에 속하는지 알 수 있다" do
+      monday_in_last_week = DayInMonth.new(1, -1)
+      nov_last_week = Date.new(2024, 11, 29)
+      expect(monday_in_last_week.week_matches nov_last_week).to eq(true)
     end
 
-    it "Date#mday 주어진 date 객체가 표현하는 날짜가 달의 몇번째 날인지 알 수 있다" do
-      monday = Date.new(2024, 11, 11)
-      expect(monday.mday).to eq(11)
+    it "주어진 date가 해당 달의 몇번째 날인지 알 수 있다" do
+      nov_11 = Date.new(2024, 11, 11)
+      expect(nov_11.mday).to eq(11)
     end
 
-    it "주어진 day_num(Date 객체가 표현하는 날짜가 해당 달에서 몇번째 날인지를 표현하는 값)이 몇번째 주에 속하는지 알 수 있다" do
+    it "주어진 day_num(date는 해당 달에서 day_num번째 날)이 몇번째 주에 속하는지 알 수 있다" do
       ignore1, ignore2 = 1, 1
       day_in_mon = DayInMonth.new(ignore1, ignore2)
 
-      (1..7).each {|first_week_day|
-        expect(day_in_mon.week_in_month first_week_day).to eq(1)
+      (1..7).each {|day_in_1st_week|
+        expect(day_in_mon.week_in_month day_in_1st_week).to eq(1)
       }
-      (8..14).each {|second_week_day|
-        expect(day_in_mon.week_in_month second_week_day).to eq(2)
+      (8..14).each {|day_in_2nd_week|
+        expect(day_in_mon.week_in_month day_in_2nd_week).to eq(2)
       }
-      (15..21).each {|third_week_day|
-        expect(day_in_mon.week_in_month third_week_day).to eq(3)
+      (15..21).each {|day_in_3rd_week|
+        expect(day_in_mon.week_in_month day_in_3rd_week).to eq(3)
       }
-      (22..28).each {|fourth_week_day|
-        expect(day_in_mon.week_in_month fourth_week_day).to eq(4)
+      (22..28).each {|day_in_4th_week|
+        expect(day_in_mon.week_in_month day_in_4th_week).to eq(4)
       }
-      (29..31).each {|fifth_week_day|
-        expect(day_in_mon.week_in_month fifth_week_day).to eq(5)
+      (29..31).each {|day_in_5th_week|
+        expect(day_in_mon.week_in_month day_in_5th_week).to eq(5)
       }
     end
 
-    it "주어진 날짜에서, 해당 달의 마지막 날까지 남은 날의 수를 구할 수 있다" do
+    it "주어진 날짜에서, 해당 달의 마지막 날까지 남은 일수를 알 수 있다" do
       ignore1, ignore2 = 1, 1
       day_in_mon = DayInMonth.new(ignore1, ignore2)
-      monday = Date.new(2024, 11, 11)
-      expect(day_in_mon.days_left_in_month monday).to eq(19)
+      nov_11 = Date.new(2024, 11, 11)
+      expect(day_in_mon.days_left_in_month nov_11).to eq(19)
     end
   end
 
