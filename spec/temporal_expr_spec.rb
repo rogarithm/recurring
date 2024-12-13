3c7cd1d54526c8e6f9b89a62145e58d823177aef
sehoon.gim <sehoongim@gmail.com>
2024-12-13 12:07:05 +0900

    테스트 코드 정리한다
     - dim -> day_in_mon

--- spec/temporal_expr_spec.rb
+++ spec/temporal_expr_spec.rb
@@ -15,21 +15,21 @@ describe "temporal expression" do
   context "day every month temporal expr을 정의할 수 있다" do
     # day of week와 count를 인자로 받는다
     it "주어진 date 객체가 몇요일인지 확인할 수 있다" do
-      dim = DayInMonth.new(1, 1)
+      day_in_mon = DayInMonth.new(1, 1)
       monday = Date.new(2024, 11, 11)
-      expect(dim.day_matches monday).to eq(true)
+      expect(day_in_mon.day_matches monday).to eq(true)
     end
 
     it "첫 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
-      dim = DayInMonth.new(1, 2)
+      day_in_mon = DayInMonth.new(1, 2)
       monday = Date.new(2024, 11, 11)
-      expect(dim.week_matches monday).to eq(true)
+      expect(day_in_mon.week_matches monday).to eq(true)
     end
 
     it "마지막 주를 기준으로 하는 cnt일 때, 주어진 date 객체가 표현하는 날짜가 달의 몇번째 주인지 확인할 수 있다" do
-      dim = DayInMonth.new(1, -1)
+      day_in_mon = DayInMonth.new(1, -1)
       friday = Date.new(2024, 11, 29)
-      expect(dim.week_matches friday).to eq(true)
+      expect(day_in_mon.week_matches friday).to eq(true)
     end
 
     it "Date#mday 주어진 date 객체가 표현하는 날짜가 달의 몇번째 날인지 알 수 있다" do
@@ -39,30 +39,30 @@ describe "temporal expression" do
 
     it "주어진 day_num(Date 객체가 표현하는 날짜가 해당 달에서 몇번째 날인지를 표현하는 값)이 몇번째 주에 속하는지 알 수 있다" do
       ignore1, ignore2 = 1, 1
-      dim = DayInMonth.new(ignore1, ignore2)
+      day_in_mon = DayInMonth.new(ignore1, ignore2)
 
       (1..7).each {|first_week_day|
-        expect(dim.week_in_month first_week_day).to eq(1)
+        expect(day_in_mon.week_in_month first_week_day).to eq(1)
       }
       (8..14).each {|second_week_day|
-        expect(dim.week_in_month second_week_day).to eq(2)
+        expect(day_in_mon.week_in_month second_week_day).to eq(2)
       }
       (15..21).each {|third_week_day|
-        expect(dim.week_in_month third_week_day).to eq(3)
+        expect(day_in_mon.week_in_month third_week_day).to eq(3)
       }
       (22..28).each {|fourth_week_day|
-        expect(dim.week_in_month fourth_week_day).to eq(4)
+        expect(day_in_mon.week_in_month fourth_week_day).to eq(4)
       }
       (29..31).each {|fifth_week_day|
-        expect(dim.week_in_month fifth_week_day).to eq(5)
+        expect(day_in_mon.week_in_month fifth_week_day).to eq(5)
       }
     end
 
     it "주어진 날짜에서, 해당 달의 마지막 날까지 남은 날의 수를 구할 수 있다" do
       ignore1, ignore2 = 1, 1
-      dim = DayInMonth.new(ignore1, ignore2)
+      day_in_mon = DayInMonth.new(ignore1, ignore2)
       monday = Date.new(2024, 11, 11)
-      expect(dim.days_left_in_month monday).to eq(19)
+      expect(day_in_mon.days_left_in_month monday).to eq(19)
     end
   end
 
