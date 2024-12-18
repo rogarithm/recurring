c7cb1b6ddded69672623b88b791a3f0536521f5d
sehoon.gim <sehoongim@gmail.com>
2024-12-18 17:52:08 +0900

    set_expr을 구현한다

---
+++ lib/set_expr/difference.rb
@@ -0,0 +1,15 @@
+class Difference
+  attr_reader :included, :excluded
+
+  def initialize included, excluded # 둘 다 set_expr 타입을 받는다
+    @included = included
+    @excluded = excluded
+  end
+
+  def includes date
+    included.includes(date) and !excluded.includes(date)
+  end
+
+  def excludes
+  end
+end
---
+++ lib/set_expr/union.rb
@@ -0,0 +1,18 @@
+class Union < Collection
+  attr_reader :elems
+
+  def initialize
+    @elems = []
+  end
+
+  def add_elem elem
+    @elems << elem
+  end
+
+  def includes date
+    @elems.each do |tmpr_expr|
+      return true if tmpr_expr.includes(date)
+    end
+    false
+  end
+end
